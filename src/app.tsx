4f5713caf0b0ebe88367900415e2818579227dc7
Felipe Araujo <devaraujofelipe@gmail.com>
2024-02-24 12:08:10 +0100

    style: add some padding to the main container

--- src/app.tsx
+++ src/app.tsx
@@ -11,7 +11,7 @@ export function App() {
   }
 
   return (
-    <main className="flex flex-1 flex-col items-center justify-center max-w-7xl min-h-screen mx-auto space-y-16 py-8">
+    <main className="flex flex-1 flex-col items-center justify-center max-w-7xl min-h-screen mx-auto space-y-16 px-4 py-8">
       <Button onClick={handleShowHotels}>Load Hotels</Button>
       {showHotels && <HotelList />}
     </main>
