0ef9e8818dc294d250464386a154d25694c20421
Felipe Araujo <devaraujofelipe@gmail.com>
2024-02-25 11:59:42 +0100

    style: improve button layout

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -4,7 +4,7 @@ export function Button({
 }: React.ButtonHTMLAttributes<HTMLButtonElement>) {
   return (
     <button
-      className={`bg-accent-500 shadow-2xl shadow-accent-400/50 font-medium min-w-56 p-4 rounded-sm text-slate-50 transition-all hover:bg-accent-600 hover:shadow-accent-400/80 ${className}`}
+      className={`bg-accent-500 font-medium line-clamp-1 h-fit min-w-56 p-4 rounded-md text-slate-50 text-lg transition-all hover:shadow-2xl hover:shadow-accent-400/80 ${className}`}
       {...props}
     />
   );
